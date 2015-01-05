276fae09d978ae2e30914592e90f3cbbb24a6605
Ben Kuper <benkuper@Bens-Mac.local>
2015-01-05 13:00:43 -0800

    cleanup git

--- NativeMIDI/ane_src/native_src/MacOS/NativeMIDI/Build/Intermediates/NativeMIDI.build/Debug/NativeMIDI.build/Objects-normal/i386/RtMidi.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/benkuper/Documents/Dev/Lib/RTMidi/RtMidi.cpp \
-  /Users/benkuper/Documents/Dev/Lib/RTMidi/RtMidi.h
--- NativeMIDI/ane_src/native_src/MacOS/NativeMIDI/Build/Intermediates/PrecompiledHeaders/NativeMIDI-Prefix-exzlzchgtxakayhejpnemydmykqw/NativeMIDI-Prefix.pch.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/benkuper/Documents/Dev/AIR-NativeExtensions/NativeMIDI/ane_src/native_src/MacOS/NativeMIDI/NativeMIDI/NativeMIDI-Prefix.pch
