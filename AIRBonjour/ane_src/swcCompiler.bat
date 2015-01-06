08a0f1f82ee23720bc1bde84b56a8da7a5b0bfb7
benkuper <bkuperberg@free.fr>
2015-01-06 14:14:03 +0100

    native src fix

--- AIRBonjour/ane_src/swcCompiler.bat
+++ AIRBonjour/ane_src/swcCompiler.bat
@@ -2,7 +2,7 @@ call %FLEX_SDK_PATH%/bin/acompc -source-path as3_src -include-classes org.opente
 
 unzip -o as3Bonjour.swc -x catalog.xml
 
-call %FLEX_SDK_PATH%/bin/adt -package -target ane as3Bonjour.ane extension-descriptor.xml -swc as3Bonjour.swc -platform Windows-x86 -C native_src\Windows-x86\Release airBonjour-win.dll -C ./ library.swf
+call %FLEX_SDK_PATH%/bin/adt -package -target ane as3Bonjour.ane extension-descriptor.xml -swc as3Bonjour.swc -platform Windows-x86 -C native_src\Windows-x86 airBonjour-win.dll -C ./ library.swf
 
 del library.swf
 del as3Bonjour.swc
